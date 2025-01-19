17de8a41961fcadaa5d44525a33043f27f482e7a
zlu-sh <dev@zlu.sh>
2025-01-19 12:34:45 -0600

    Add clock object and deltatime variable

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from constants import *
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while ( True ):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)
     
 if __name__ == "__main__":
     main()
