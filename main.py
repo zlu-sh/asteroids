5f865380093066e5db29f6d35895f09615e2dcfd
zlu-sh <dev@zlu.sh>
2025-01-18 19:34:13 -0600

    Print screen resolution

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import constants
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
     
 if __name__ == "__main__":
     main()
