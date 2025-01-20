b8a092d2ef02f26aad9eff0d742c80f954dbf4ef
zlu-sh <dev@zlu.sh>
2025-01-20 08:10:31 -0600

    Add movement controls to player

--- player.py
+++ player.py
@@ -29,3 +29,10 @@ class Player(CircleShape):
             self.rotate(-1 * dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-1 * dt)
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
