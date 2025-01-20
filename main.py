6dcca71f63bb761e315ac604c170ce642e64d22e
zlu-sh <dev@zlu.sh>
2025-01-20 08:06:04 -0600

    Add rotation controls to player

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
+
         screen.fill("black")
-
         player.draw(screen)
-
         pygame.display.flip()
-        dt = clock.tick(60)
+
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import *
 import pygame
 
 class Player(CircleShape):
@@ -18,5 +18,14 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_a]:
+            self.rotate(-1 * dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
